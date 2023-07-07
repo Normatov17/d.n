efb4bbee6dd6e75d13ea92b6183c41c1122b2226
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-07-07 20:20:13 +0500

    Equality Operators

---
+++ app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// let js = "JavaScript";
+
+// if (js === "JavaScript") {
+//   alert("Hello Word!");
+// } else {
+//   alert("Bye Bye Word!");
+// }
+
+// ism = "d";
+// console.log(ism);
+
+// let ism = "Davlatbek";
+// let familya = "Normatov";
+// const yosh = 19;
+
+// const fullName = ism + " " + familya + " " + yosh;
+
+// console.log(fullName);
+
+// const fullName2 = ism.concat(familya).concat(yosh);
+// console.log(fullName);
+
+// let fristName = "Davlatbek";
+// console.log(fristName);
+
+// // var let and conct
+// var ism = "DAVLATBEK";
+// ism = "DN";
+// console.log(ism);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 95;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heightJohn;
+// console.log(BMIJohn, BMIMark);
+
+// console.time();
+// const name = "Davlatbek";
+// const job = "Devoloper";
+// const ful = `${name} ${job}`;
+// console.log(ful);
+
+// console.log("22" - "12");
+// console.timeEnd();
+
+// Equality Operators
+
+// const ism = prompt();
+
+// if (ism === "Davlatbek") {
+//   alert("Qalesiz endi");
+// }
