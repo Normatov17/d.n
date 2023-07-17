62791115d9b97f8479a9b3977486ac4cc767b74a
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-07-17 18:27:06 +0500

    clonlash

--- cammi/sammi.js
+++ cammi/sammi.js
@@ -94,49 +94,227 @@
 // Funksiyalar
 
 // // Amaliyot Amaliy Mashq
-let numberOfSeries;
+// let numberOfSeries;
 
-function startApp() {
-  numberOfSeries = +prompt("Nechta Serial Ko'rdingiz", "");
+// function startApp() {
+//   numberOfSeries = +prompt("Nechta Serial Ko'rdingiz", "");
 
-  while (
-    numberOfSeries == "" ||
-    numberOfSeries == null ||
-    isNaN(numberOfSeries)
-  ) {
-    numberOfSeries = +prompt("Nechta Serial Ko'rdingiz", "");
+//   while (
+//     numberOfSeries == "" ||
+//     numberOfSeries == null ||
+//     isNaN(numberOfSeries)
+//   ) {
+//     numberOfSeries = +prompt("Nechta Serial Ko'rdingiz", "");
+//   }
+// }
+
+// startApp();
+// const seriesDB = {
+//   count: numberOfSeries,
+//   series: {},
+//   actors: [],
+//   genres: [],
+//   privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Oxirgi ko'rgan serialingiz?");
+//   const b = prompt("Necha baxo berasiz?");
+//   if (a != null && b != null && a != "" && b != "") {
+//     seriesDB.series[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+
+// if (seriesDB.count < 5) {
+//   console.log("Kam Serial ko'ribsiz");
+// } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+//   console.log("Siz klassik tomashabin ekansiz");
+// } else if (seriesDB.count >= 10) {
+//   console.log("Siz serialchi zvezda ekansiz");
+// } else {
+//   // console.error("Error");
+// }
+
+// function showDb() {
+//   if (seriesDB.privat === seriesDB.privat) {
+//     console.log(seriesDB);
+//   } else {
+//     console.log("Aksincha");
+//   }
+// }
+
+// showDb();
+
+// function writeGenres(janr) {
+//   for (let i = 0; i <= 2; i++) {
+//     seriesDB.genres[i] = prompt(`Yaxshi ko'rgan janringiz ${i + 1}`);
+//   }
+// }
+
+// writeGenres();
+
+//Calback
+// function edu(sumject, callback) {
+//   console.log(`I wanna teach ${sumject}`);
+//   callback();
+// }
+
+// edu("Ingliz tile", function () {
+//   console.log(`That's great`);
+// });
+
+// function frist(call) {
+//   setTimeout(() => {
+//     console.log(1);
+//     call();
+//   }, 1000);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// frist(second);
+
+// Object
+// const theif = {
+//   jacket: "black",
+//   height: "1.70",
+//   colors: {
+//     hair: "gray",
+//     style: "curley",
+//   },
+//   howOut: function () {
+//     console.log(`Fast with help doors🚪`);
+//   },
+// };
+
+// const { hair, style } = theif.colors;
+// console.log(hair);
+// console.log(style);
+
+// theif.howOut();
+
+// console.log(theif);
+
+// delete theif.jacket;
+
+// console.log(Object.keys(theif));
+// console.log(Object.values(theif));
+
+// for (let key in theif) {
+//   if (theif[key] === "object") {
+//     for (let i in theif[key]) {
+//       console.log(theif[key][i]);
+//     }
+//   } else {
+//     console.log(key);
+//   }
+// }
+
+// Amaliyot
+
+// const user = {
+//   fristName: "Davlatbek",
+//   lastName: "Normatov",
+//   age: 19,
+//   languages: ["uzbek", "russian", "english", "kirgiz"],
+//   location: "Uzbekistan, Andijan",
+//   speek: function () {
+//     this.languages.forEach((leng) => {
+//       console.log(`I'am speeking ${leng}`);
+//     });
+//   },
+// };
+
+// const { fristName, lastName } = user;
+// console.log(fristName);
+// console.log(lastName);
+// user.speek();
+
+//  TODO Masszivlar
+
+// const arr = [1, 12, 73, 14, 35, 26, 17, 8, 9];
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+// delete arr[1];
+// console.log(arr);
+// arr.push(10);
+// console.log(arr);
+// arr.unshift(0);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let values of arr) {
+//   console.log(values);
+// }
+
+// arr.forEach((item, i) => {
+//   console.log(`${i}: ${item}`);
+// });
+
+// const movies = prompt("What's your favourite movies!", "");
+// const userMovie = movies.split(", ");
+// userMovie.sort();
+// console.log(userMovie.join("- "));
+
+// Clonlash ES6 -- Copy
+// const x = 5;
+// const y = x;
+// console.log(y);
+// console.log(x);
+
+// const user = {
+//   fristName: "Davlatbek",
+//   lastName: "Normatov",
+//   age: 19,
+//   languages: ["uzbek", "russian", "english", "kirgiz"],
+//   location: "Uzbekistan, Andijan",
+//   job: "Progammer",
+//   speek: function () {
+//     this.languages.forEach((leng) => {
+//       console.log(`I'am speeking ${leng}`);
+//     });
+//   },
+// };
+
+// const user2 = { ...user };
+// console.log(user2);
+
+// function copy(user) {
+//   console.log(
+//     `Mening ismin ${user.fristName} familyam ${user.lastName} yoshim ${user.age} va men ${user.job}man`
+//   );
+// }
+
+// copy(user);
+
+function nusxaObj(obj) {
+  let ObjNusxa = {};
+
+  for (let key in obj) {
+    ObjNusxa[key] = obj[key];
   }
+
+  return ObjNusxa;
 }
 
-startApp();
-const seriesDB = {
-  count: numberOfSeries,
-  series: {},
-  actors: [],
-  genres: [],
-  privat: false,
+const number = {
+  x: 10,
+  y: 5,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Oxirgi ko'rgan serialingiz?");
-  const b = prompt("Necha baxo berasiz?");
-  if (a != null && b != null && a != "" && b != "") {
-    seriesDB.series[a] = b;
-    console.log("done");
-  } else {
-    console.log("error");
-    i--;
-  }
-}
+const newNumber = nusxaObj(number);
 
-if (seriesDB.count < 5) {
-  console.log("Kam Serial ko'ribsiz");
-} else if (seriesDB.count >= 5 && seriesDB.count < 10) {
-  console.log("Siz klassik tomashabin ekansiz");
-} else if (seriesDB.count >= 10) {
-  console.log("Siz serialchi zvezda ekansiz");
-} else {
-  console.error("Error");
-}
-
-console.log(seriesDB);
+newNumber.x = 20;
+console.log(newNumber);
+console.log(number);
