1b18dbeec889687cdb5a5405c9ccbae35ef02bbb
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-08-27 21:09:53 +0300

    classlist

--- DOM/script.js
+++ DOM/script.js
@@ -1,43 +1,12 @@
-// document.addEventListener("DOMContentLoaded", (e) => {
-//   // alert("Hello");
+// const blue = document.querySelectorAll("button");
 
-//   const box = document.querySelector("#box");
-//   const buttons = document.querySelectorAll("button");
-//   const open = document.querySelector(".open");
-//   const crculWrapper = document.querySelector(".circle__wrapper");
-//   const cri = document.querySelectorAll(".circle");
-
-//   buttons.forEach((button) => {
-//     // button.style.cssText = "marginTop: 20px; color: red; background: blue";
-//     button.addEventListener("mouseover", (e) => {
-//       button.classList.toggle("open");
-//       button.scrollTop = "100px".fixed();
-//     });
-//   });
-
-//   const butt = document.createElement("div");
-//   crculWrapper.append(butt);
-//   butt.classList.add("circle");
-//   cri[0].replaceWith(butt);
-//   butt.innerHTML = `<b>0</b>`;
-//   // box.style.margin = "20px";
+// blue[0].addEventListener("click", () => {
+//   // if (!blue[1].classList.contains("red")) {
+//   //   blue[1].classList.toggle("red");
+//   // } else {
+//   // blue[1].classList.remove("red");
+//   // }
+//   blue[1].classList.toggle("red");
 // });
 
-const link = document.querySelector(".link");
-// link.addEventListener("click", (e) => {
-//   e.target.remove();
-// });
-
-let i = 0;
-const addElement = (evnt) => {
-  i++;
-  if (i === 10) {
-    link.removeEventListener("click", addElement);
-  }
-  console.log(i);
-};
-
-link.addEventListener("click", addElement);
-
-console.log(document.head);
-console.log(document.body.childNodes);
+//  Delegatsiya
